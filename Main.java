2d6d6d29605425aa0eace236ebd90857123aa390
Yacinello-droid <136171795+Yacinello-droid@users.noreply.github.com>
2023-11-17 04:28:43 +0500

    Days in every month
    Are you forgetting how much days in any month? Me too! So I've solved this problem and made this reminder! Just open this file thought console and type count number of month that you need(1-12)

---
+++ Main.java
@@ -0,0 +1,46 @@
+package IDK;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Month = sc.nextInt();
+        String Days;
+        if(Month == 1) {
+            System.out.println(31);
+        }
+        if(Month == 2) {
+            System.out.println(28);
+        }
+        if(Month == 3) {
+            System.out.println(31);
+        }
+        if(Month == 4) {
+            System.out.println(30);
+        }
+        if(Month == 5) {
+            System.out.println(31);
+        }
+        if(Month == 6) {
+            System.out.println(30);
+        }
+        if(Month == 7) {
+            System.out.println(31);
+        }
+        if(Month == 8) {
+            System.out.println(31);
+        }
+        if(Month == 9) {
+            System.out.println(30);
+        }
+        if(Month == 10) {
+            System.out.println(31);
+        }
+        if(Month == 11) {
+            System.out.println(30);
+        }
+        if(Month == 12) {
+            System.out.println(31);
+        }
+    }
+}
