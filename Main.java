822f53fe38b6bfaafdb94ff59b6d0f3bc5527e88
Max <44063879+tapchik@users.noreply.github.com>
2023-11-29 13:42:41 +0500

    Slightly modified

--- Main.java
+++ Main.java
@@ -3,44 +3,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        int Month = sc.nextInt();
-        String Days;
-        if(Month == 1) {
-            System.out.println(31);
-        }
-        if(Month == 2) {
-            System.out.println(28);
-        }
-        if(Month == 3) {
-            System.out.println(31);
-        }
-        if(Month == 4) {
-            System.out.println(30);
-        }
-        if(Month == 5) {
-            System.out.println(31);
-        }
-        if(Month == 6) {
-            System.out.println(30);
-        }
-        if(Month == 7) {
-            System.out.println(31);
-        }
-        if(Month == 8) {
-            System.out.println(31);
-        }
-        if(Month == 9) {
-            System.out.println(30);
-        }
-        if(Month == 10) {
-            System.out.println(31);
-        }
-        if(Month == 11) {
-            System.out.println(30);
-        }
-        if(Month == 12) {
-            System.out.println(31);
-        }
+
+        System.out.print("Enter a number for a month: ");
+        int month = sc.nextInt();
+        int days = 0; 
+
+        if (month == 2 ) {
+            days = 28;
+        } else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+            days = 31;
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            days = 30;
+        } 
+
+        if (days != 0)
+            System.out.println("Number of days in " + month + " month is: " + days);
+        else
+            System.out.println("Invalid month, no answer can be provided");
     }
 }
